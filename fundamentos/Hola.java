741a0a1ba9cd210b2623a8b74eb00fd3ca13fdc1
johana <johanarodriguez310@gmail.com>
2024-10-02 10:07:29 -0600

    int valor 20 en Hola.java

--- fundamentos/Hola.java
+++ fundamentos/Hola.java
@@ -2,7 +2,7 @@ public class Hola {
     public static void main(String[] args) {
         System.out.println("Hola Mundo - Grupo 52");
         int valor1 = 10;
-        int valor2 = 0;
+        int valor2 = 20;
         int operacionSuma = valor1 + valor2;
         int operacionResta = valor1 - valor2;
         int operacionMultiplicacion = valor1 * valor2;
